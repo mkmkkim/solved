5c222ddbed375fa1330dfbb008ffcc76e07415e0
mkmkkim <0098121@naver.com>
2022-02-25 21:49:23 +0900

    Create solved.cpp

---
+++ solved/solved/solved.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int d, w, h;
+	cin >> d >> h >> w;
+	if (w < 2 || w>100 || h < 1 || h > 99 || 5 > d || d > 1000) {
+		return 0;
+	}
+	if (h < w) {
+		double a = pow(d, 2) / (pow(h, 2) + pow(w, 2));
+		cout << int(sqrt(a) * h) << " ";
+		cout << int(sqrt(a) * w) << " ";
+	}
+}
